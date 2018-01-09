81c83d37823152e65b3b6fd27d0cae6c3b11b8bd
Iwan Winoto <iwinoto@au1.ibm.com>
2018-01-09 15:47:25 +1100

    Parameters

--- kubernetes/kube-imagePullSecret.sh
+++ kubernetes/kube-imagePullSecret.sh
@@ -1,2 +1,6 @@
-kubectl --namespace car-dashboard create secret docker-registry registry-ng-funfactory --docker-server=registry.ng.bluemix.net --docker-username=token --docker-password=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMDAwMTc3Zi03Mjk4LTU4NDktYmYyMi00ZDUwN2ZkOTQwYmUiLCJpc3MiOiJyZWdpc3RyeS5ibHVlbWl4Lm5ldCJ9.FUwUmPT9RRfrjp8DboYXP2od2bF1gPOwMLR1maYD5Bs --docker-email=iwinoto@gmail.com
+# Ref: https://console.bluemix.net/docs/containers
+# $1 = namespace
+# $2 = Docker key
+# $3 = email address
+kubectl --namespace $1 create secret docker-registry registry-ng-funfactory --docker-server=registry.ng.bluemix.net --docker-username=token --docker-password=$2 --docker-email=
 
