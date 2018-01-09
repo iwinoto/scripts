d2cdaf7aca2f1ee31bb3a031e7f76ce35806d62e
Iwan Winoto <iwinoto@au1.ibm.com>
2018-01-09 15:44:27 +1100

    formatting

--- CloudPrivate/set-docker-registry-secret.sh
+++ CloudPrivate/set-docker-registry-secret.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Create Docker config registry Secret
-kubectl -n iwinoto create secret docker-registry docker-admin.registrykey --docker-server=https://mycluster.icp:8500 --docker-username=admin --docker-password=admin --docker-email=null
+kubectl -n iwinoto create secret docker-registry docker-admin.registrykey \
+  --docker-server=https://mycluster.icp:8500 \
+  --docker-username=admin --docker-password=admin \
+  --docker-email=null
 
 # Add image registry secret as ImagePullSecret in default service account
-kubectl -n iwinoto get serviceaccounts default -o yaml | \
-sed -e "/resourceVersion.*/d"\
-  -e "$ a imagePullSecrets\n- name: docker-admin.registrykey" - | \
-kubectl -n iwinoto replace serviceaccount default -f -
+kubectl -n iwinoto get serviceaccounts default -o yaml \
+  | \
+  sed -e "/resourceVersion.*/d"\
+    -e "$ a imagePullSecrets\n- name: docker-admin.registrykey" - \
+  | \
+  kubectl -n iwinoto replace serviceaccount default -f -
--- ContainerService/kube-imagePullSecret.sh
+++ ContainerService/kube-imagePullSecret.sh
@@ -1,2 +1,5 @@
-kubectl --namespace car-dashboard create secret docker-registry registry-ng-funfactory --docker-server=registry.ng.bluemix.net --docker-username=token --docker-password=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMDAwMTc3Zi03Mjk4LTU4NDktYmYyMi00ZDUwN2ZkOTQwYmUiLCJpc3MiOiJyZWdpc3RyeS5ibHVlbWl4Lm5ldCJ9.FUwUmPT9RRfrjp8DboYXP2od2bF1gPOwMLR1maYD5Bs --docker-email=iwinoto@gmail.com
-
+kubectl --namespace car-dashboard create secret docker-registry registry-ng-funfactory \
+   --docker-server=registry.ng.bluemix.net \
+   --docker-username=token \
+   --docker-password=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMDAwMTc3Zi03Mjk4LTU4NDktYmYyMi00ZDUwN2ZkOTQwYmUiLCJpc3MiOiJyZWdpc3RyeS5ibHVlbWl4Lm5ldCJ9.FUwUmPT9RRfrjp8DboYXP2od2bF1gPOwMLR1maYD5Bs \
+   --docker-email=iwinoto@gmail.com
