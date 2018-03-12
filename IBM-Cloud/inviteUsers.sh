61be1a3a6b30e2768c6a8782641319680bd16817
Iwan Winoto <iwinoto@au1.ibm.com>
2018-03-12 14:40:05 +1100

    Formalised arguments & options. Refined functionality

--- IBM-Cloud/inviteUsers.sh
+++ IBM-Cloud/inviteUsers.sh
@@ -1,11 +1,105 @@
 #!/bin/bash
 
-USERS=$1
-ORG=$2
-SPACE=$3
+USERS=
+ORG=
+SPACE=dev
 REGION="au-syd"
 ORG_ROLES="OrgManager BillingManager OrgAuditor"
 SPACE_ROLES='SpaceManager SpaceDeveloper SpaceAuditor'
+
+print_help()
+{
+  echo
+  echo "Invite users to an IBM Cloud Organisation"
+  echo "Pre-requisite: You MUST be logged in to IBM Cloud with 'bx login'."
+  echo
+  echo "Usage:"
+  echo "$0 [-s <Default Org space> -r <IBM Cloud region>] ORG-NAME USERS"
+  echo
+  echo "Arguments:"
+  echo "ORG-NAME  IBM Cloud Organisation (Org) to replicate."
+  echo "Users     Space separated list of users to invite to Org."
+  echo "          Users will be provided with the following roles:"
+  echo "          ORG ROLES:"
+  for ROLE in $ORG_ROLES
+  do
+    printf '%b' "\t\t$ROLE\n"
+  done
+  echo "          SPACE ROLES:"
+  for ROLE in $SPACE_ROLES
+  do
+    printf '%b' "\t\t$ROLE\n"
+  done
+  echo
+  echo "Options are:"
+  echo "-u  [REQUIRED]  Space separated list of users to invite to Org."
+  echo "                Users will be provided with the following roles:"
+  for ROLE in $SPACE_ROLES
+  do
+    printf '%b' "\t\t\t$ROLE\n"
+  done
+  echo "-o  [REQUIRED]  IBM Cloud Organisation (Org) to invite users to."
+  echo "-s  [OPTIONAL]  Default space to add to replicated Org."
+  echo "                Defaults to 'dev'"
+  echo "-r  [OPTIONAL]  IBM Cloud region of Org to invite users to."
+  echo "                Defaults to currently logged in region."
+  echo "                Must be one of:"
+  printf '%b' "`(bx regions | awk ''/.-./' {print "\t\t\t",$1}' -)`\n"
+  echo "-h  Print this message."
+  echo
+  exit 0
+}
+
+while getopts "s:r:h" optname
+do
+    case "$optname" in
+      "s")
+        SPACE=$OPTARG
+        ;;
+      "r")
+        REGION=$OPTARG
+        ;;
+      "h")
+        print_help
+        exit $?
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        print_help
+        exit $?
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        print_help
+        exit $?
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        print_help
+        exit $?
+        ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+if [ -n "$1" ]; then
+  ORG="$1"
+else
+  echo "Incorrect number of arguments."
+  echo "You must provide ORG-NAME and USERS."
+  print_help
+  exit 1
+fi
+if [ -n "$2" ]; then
+  USERS="$2"
+else
+  echo "Incorrect number of arguments."
+  echo "You must provide ORG-NAME and USERS."
+  print_help
+  exit 1
+fi
 
 bx account org-create $ORG
 bx target -o $ORG
--- IBM-Cloud/replicateOrg.sh
+++ IBM-Cloud/replicateOrg.sh
@@ -1,27 +1,116 @@
 #!/bin/bash
 
-ORG=$1
-USERS=$2
+ORG=
+USERS=
 
 SPACE=dev
-#REGIONS='au-syd eu-de eu-gb us-east us-south'
 REGIONS=$(bx regions | awk ''/.-./' {print $1}' -)
 SPACE_ROLES='SpaceManager SpaceDeveloper SpaceAuditor'
+
+print_help()
+{
+  echo
+  echo "Replice an IBM Cloud Organisation to other regions."
+  echo "Pre-requisite: You MUST be logged in to IBM Cloud with 'bx login'."
+  echo
+  echo "Usage:"
+  echo "$0 [-u <Users> -s <Default Org space> -r <IBM Cloud region>] ORG-NAME"
+  echo
+  echo "Arguments:"
+  echo "ORG-NAME  IBM Cloud Organisation (Org) to replicate."
+  echo
+  echo "Options are:"
+  echo "-u  [OPTIONAL]  Space separated list of users in Org to provide"
+  echo "                space roles to."
+  echo "                Defaults to ALL users in the Org."
+  echo "                The following space roles will be provided:"
+  for ROLE in $SPACE_ROLES
+  do
+    printf '%b' "\t\t\t$ROLE\n"
+  done
+  echo "-s  [OPTIONAL]  Default space to add to replicated Org."
+  echo "                Defaults to 'dev'"
+  echo "-r  [OPTIONAL]  Space separated list of IBM Cloud regions to"
+  echo "                replicate Org to."
+  echo "                Defaults to all regions."
+  echo "                Must be at least one of:"
+  printf '%b' "`(bx regions | awk ''/.-./' {print "\t\t\t",$1}' -)`\n"
+  echo "-h  Print this message."
+  echo
+}
+
+while getopts "u:s:r:h" optname
+do
+    case "$optname" in
+      "u")
+        USERS=$OPTARG
+        ;;
+      "s")
+        SPACE=$OPTARG
+        ;;
+      "r")
+        REGIONS=$OPTARG
+        ;;
+      "h")
+        print_help
+        exit 0
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        print_help
+        exit 1
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        print_help
+        exit 1
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        print_help
+        exit $?
+        ;;
+    esac
+done
+
+shift $(($OPTIND -1))
+if [ -n $1 ]; then
+  ORG=$1
+else
+  echo "Argument ORG-NAME missing."
+  print_help
+  exit 1
+fi
+
+if [ -z "$USERS" ]; then
+  echo "No users provided. Getting all users for Org $ORG."
+  USERS=$(bx account org-users $ORG -a | awk 'NR>2 {print $1}' -)
+
+  for USER in $USERS
+  do
+    echo "    $USER"
+  done
+fi
 
 for REGION in $REGIONS
 do
   echo replicating org $ORG to region $REGION
 
   bx account org-replicate $ORG $REGION
-  bx target -r $REGION -o $ORG
-  bx account space-create $SPACE
 
-  for USER in $USERS
-  do
-    echo Setting space roles for user = $USER
-    for ROLE in $SPACE_ROLES
+  if [ -n "$SPACE" ]
+    echo "Adding space $SPACE to Org $ORG in region $REGION"
+    bx target -r $REGION -o $ORG
+    bx account space-create $SPACE
+
+    for USER in $USERS
     do
-      bx account space-role-set $USER $ORG $SPACE $ROLE
+      echo Setting space roles for user $USER
+      for ROLE in $SPACE_ROLES
+      do
+        bx account space-role-set $USER $ORG $SPACE $ROLE
+      done
     done
-  done
+  fi
 done
