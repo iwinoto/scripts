1bfc178afddbfddc1105ea2b96153273114fd37a
Iwan Winoto <iwinoto@au1.ibm.com>
2018-11-28 21:10:10 +1100

    ICP

--- CloudPrivate/set-docker-registry-secret.sh
+++ CloudPrivate/set-docker-registry-secret.sh
@@ -2,14 +2,14 @@
 
 # Create Docker config registry Secret
 kubectl -n iwinoto create secret docker-registry docker-admin.registrykey \
-  --docker-server=https://mycluster.icp:8500 \
-  --docker-username=admin --docker-password=admin \
+  --docker-server=https://icpcluster.icp:8500 \
+  --docker-username=admin --docker-password=passw0rd \
   --docker-email=null
 
 # Add image registry secret as ImagePullSecret in default service account
 kubectl -n iwinoto get serviceaccounts default -o yaml \
   | \
   sed -e "/resourceVersion.*/d"\
-    -e "$ a imagePullSecrets\n- name: docker-admin.registrykey" - \
+    -e "$ a imagePullSecrets:\n- name: docker-admin.registrykey" - \
   | \
   kubectl -n iwinoto replace serviceaccount default -f -
